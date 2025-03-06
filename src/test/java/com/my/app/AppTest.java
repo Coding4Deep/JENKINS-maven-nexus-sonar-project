53c13560b840bc55d1cbb4c8a5c443a90e92f787
DEEPAK SAGAR <sagardeepak2002@gmail.com>
2025-03-06 14:44:00 +0530

    Update AppTest.java

--- src/test/java/com/my/app/AppTest.java
+++ src/test/java/com/my/app/AppTest.java
@@ -1,7 +1,7 @@
 package com.my.app;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test; // Update this import for JUnit 5
+import static org.junit.jupiter.api.Assertions.assertTrue; // Update assertion import
 
 /**
  * Unit test for simple App.
